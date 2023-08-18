583869cfb0ea3f10accf7d82ce264e5e80990979
Crowdbotics <team@crowdbotics.com>
2023-08-18 13:26:11 +0000

    Initial commit.

---
+++ backend/bookaffordableride_42933/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for bookaffordableride_42933 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookaffordableride_42933.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bookaffordableride-42933",
+  url: "https://bookaffordableride-42933.botics.co",
+  api: "https://bookaffordableride-42933.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
